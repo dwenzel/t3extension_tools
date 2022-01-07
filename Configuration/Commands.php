a85b664f3b30d7dbfbea36b29ca035f430162af9
Dirk Wenzel <wenzel@cps-it.de>
2022-01-07 10:23:04 +0100

    [BUGFIX] example command registration corrected

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -2,10 +2,7 @@
 declare(strict_types=1);
 
 return [
-    \CPSIT\T3importExport\Command\ImportSetCommand::DEFAULT_NAME => [
-        'class' => \CPSIT\T3importExport\Command\ImportSetCommand::class,
-    ],
-    \CPSIT\T3importExport\Command\ImportTaskCommand::DEFAULT_NAME => [
-        'class' => \CPSIT\T3importExport\Command\ImportTaskCommand::class,
-    ],
+    \DWenzel\T3extensionTools\Command\ExampleCommand::DEFAULT_NAME => [
+        'class' => \DWenzel\T3extensionTools\Command\ExampleCommand::class
+    ]
 ];
