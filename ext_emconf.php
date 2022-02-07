f5964b300d5382be2e48266ada428869aee2abfd
Dirk Wenzel <wenzel@cps-it.de>
2022-02-07 22:16:18 +0100

    [RELEASE] Release 1.7.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.6.0',
+	'version' => '1.7.0',
 	'constraints' =>
 	array (
 		'depends' =>
