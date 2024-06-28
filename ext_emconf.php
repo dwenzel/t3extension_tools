7037486fdfb61436334ce84159d08fcb3b96b98d
Marno Schmigotzki <m.schmigotzki@familie-redlich.de>
2024-06-28 16:34:02 +0200

    [TASK] optimize requirements/dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99'
+            'typo3' => '11.5.0-12.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
